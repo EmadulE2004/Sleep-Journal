c85fa81fccb26f89da6833599836c59a60f78882
EmadulE2004 <emadnoor898@gmail.com>
2025-05-28 20:51:24 -0400

    moved stuff around

--- Frontend/sleep-journal/App.js
+++ Frontend/sleep-journal/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Homescreen} from './screens/HomeScreen';
+import {Homescreen} from './screens/Homescreen';
 import {AICHAT} from './screens/AICHAT';
 import {LogScreen} from './screens/LogScreen';
 import {Profile} from './screens/Profile';
+
+
