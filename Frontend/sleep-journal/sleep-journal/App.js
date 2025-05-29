cf115a038ada04b836adb7e3b0866623490af90c
Emadul Emad <148724516+EmadulE2004@users.noreply.github.com>
2025-05-28 22:02:28 -0400

    stuff

--- Frontend/sleep-journal/sleep-journal/App.js
+++ Frontend/sleep-journal/sleep-journal/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Homescreen} from './screens/Homescreen';
-import {AICHAT} from './screens/AICHAT';
-import {LogScreen} from './screens/LogScreen';
-import {Profile} from './screens/Profile';
+import {Homescreen} from '../screens/Homescreen';
+import {AICHAT} from '../screens/AICHAT';
+import {LogScreen} from '../screens/LogScreen';
+import {Profile} from '../screens/Profile';
 
 
 const Tab = createBottomTabNavigator();
