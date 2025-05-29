cdb14948b75fac42dcd8469e2e5f8208ceaa53e9
EmadulE2004 <emadnoor898@gmail.com>
2025-05-28 20:48:24 -0400

    added dependencies

---
+++ Frontend/sleep-journalapp/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Homescreen} from './screens/HomeScreen';
+import {AICHAT} from './screens/AICHAT';
+import {LogScreen} from './screens/LogScreen';
+import {Profile} from './screens/Profile';
