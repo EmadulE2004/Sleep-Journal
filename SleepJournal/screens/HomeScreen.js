6b733a7f7e3bc746db92510d604a05b2d664120f
Mahfuz Ahmed <128321890+mahfuzmahi@users.noreply.github.com>
2025-06-10 21:37:22 -0700

    Added icons and navigation bar to the homepage

--- SleepJournal/screens/HomeScreen.js
+++ SleepJournal/screens/HomeScreen.js
@@ -1,20 +1,66 @@
 // src/screens/HomeScreen.js (or just screens/HomeScreen.js)
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity, Image } from 'react-native';
 
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome Home!</Text>
-      <Button title="Go to Profile" onPress={() => navigation.navigate('Profile')} />
-      <Button title="Logout" onPress={() => navigation.replace('Lock')} />
+        <View>
+            <Text style={styles.title}>Welcome Home!</Text>
+            <Button title="Logout" onPress={() => navigation.replace('Lock')} />
+        </View>
+
+        <View style = {styles.navBar}>
+            <TouchableOpacity style = {styles.navIcon}>
+                <Image source = {require('../assets/icons/home.png')} style={styles.icon}/>
+            </TouchableOpacity>
+
+            <TouchableOpacity style = {styles.navIcon}>
+                <Image source = {require('../assets/icons/journal.png')} style={styles.icon}/>
+            </TouchableOpacity>
+
+            <TouchableOpacity style = {styles.navIcon}>
+                <Image source = {require('../assets/icons/graph.png')} style={styles.icon}/>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => navigation.navigate('Profile')} style = {styles.navIcon}>
+                <Image source = {require('../assets/icons/user.png')} style={styles.icon}/>
+            </TouchableOpacity>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  title: { fontSize: 24, marginBottom: 20 },
+    container: { 
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+    },
+    
+    title: { 
+        fontSize: 24, 
+        marginBottom: 20 
+    },
+    
+    navBar: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: 'white',
+        paddingVertical: 14,
+        borderTopWidth: 1,
+        borderTopColor: 'black'
+    },
+
+    navIcon: {
+        alignItems: 'center'
+    },
+
+    icon: {
+        width: 50,
+        height: 50,
+        resizeMode: 'contain'
+    }
 });
 
 export default HomeScreen;
