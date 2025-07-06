e06c4a1ea3f5e5df6451b462070c323737cdf800
Mahfuz Ahmed <128321890+mahfuzmahi@users.noreply.github.com>
2025-07-05 17:27:20 -0700

    Return function created

--- SleepJournal/screens/MoodSelection.js
+++ SleepJournal/screens/MoodSelection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Modal, Text, TouchableOpacity, Image } from "react-native";
+import { View, Modal, Text, TouchableOpacity, Image, StyleSheet} from "react-native";
 
 export default function MoodSelection({visible, select, close}) {
     const mood = [
@@ -8,5 +8,41 @@ export default function MoodSelection({visible, select, close}) {
         {name: 'sad', image: require('../assets/icons/sad.png')},
         {name: 'tired', image: require('../assets/icons/tired.png')},
         {name: 'angry', image: require('../assets/icons/angry.png')}
-    ]
-} 
+    ];
+
+    return (
+        <Modal
+            animationType = "fade"
+            transparent
+            visible = {visible}
+            onRequestClose = {close}
+        >
+            <View style={styles.overlay}>
+                <View style={styles.box}>
+                    <Text style={styles.title}>
+                         How are you feeling?
+                    </Text>
+                    <View style={styles.row}>
+                        {mood.map((moods, idx) => (
+                        <TouchableOpacity
+                            key={idx}
+                            style={styles.moodItem}
+                            onPress={() => select(moods.name)}
+                        >
+                            <Image source={moods.image} style={styles.moodImage}/>
+                            <Text style={styles.label}>
+                                {moods.name}
+                            </Text>
+                        </TouchableOpacity>
+                        ))}
+                    </View>
+                </View>
+            </View>
+        </Modal>
+
+    )
+};
+
+const styles = StyleSheet.create({
+
+})
