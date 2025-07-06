4b71f7df74000b4e0976eb3b2c2072fdc5c34a05
Mahfuz Ahmed <128321890+mahfuzmahi@users.noreply.github.com>
2025-07-05 17:01:53 -0700

    Connect designated icons to variable

--- SleepJournal/screens/MoodSelection.js
+++ SleepJournal/screens/MoodSelection.js
@@ -3,10 +3,10 @@ import { View, Modal, Text, TouchableOpacity, Image } from "react-native";
 
 export default function MoodSelection({visible, select, close}) {
     const mood = [
-        {name: 'happy'},
-        {name: 'neutral'},
-        {name: 'sad'},
-        {name: 'tired'},
-        {name: 'angry'}
+        {name: 'happy', image: require('../assets/icons/happy.png')},
+        {name: 'neutral', image: require('../assets/icons/neutral.png')},
+        {name: 'sad', image: require('../assets/icons/sad.png')},
+        {name: 'tired', image: require('../assets/icons/tired.png')},
+        {name: 'angry', image: require('../assets/icons/angry.png')}
     ]
 } 
