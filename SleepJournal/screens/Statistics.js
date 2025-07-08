a91cae49e6ee558a763faaddd11ec9c2e4516d53
Emadul Emad <148724516+EmadulE2004@users.noreply.github.com>
2025-07-08 12:51:41 -0400

    nothing new

--- SleepJournal/screens/Statistics.js
+++ SleepJournal/screens/Statistics.js
@@ -1,10 +1,9 @@
-import React from 'react';
 import { View, Text } from 'react-native';
 
 export default function Statistics() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Sleep Analytics</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'Top' }}>
+      <Text>Sleep Analyticss</Text>
     </View>
   );
 }
