fe65cde1e2eaba246c1ca10d8d3604717fce1018
Mahfuz Ahmed <128321890+mahfuzmahi@users.noreply.github.com>
2025-07-08 15:09:06 -0700

    Added rating and score logic

--- SleepJournal/screens/SleepScore.js
+++ SleepJournal/screens/SleepScore.js
@@ -9,22 +9,57 @@ export default function SleepScore({score = 90}) {
     const c = 2 * Math.PI * r;
     const p = (score / 100) * c;
 
-    return (
-        <View>
-            <Text></Text>
+    const rating = () => {
+        if(score >= 90) {
+            return "Excellent";
+        } else if(score >= 70) {
+            return "Good";
+        } else if(score >= 50) {
+            return "Average";
+        } else {
+            return "Poor";
+        }
+    }
 
-            <View>
+    return (
+        <View style = {styles.container}>
+            <Text style = {styles.title}>Sleep Score</Text>
+
+            <View style = {styles.wrapper}>
                 <Svg>
-                    <Circle/>
-                    <Circle/>
+                    <Circle
+                        stroke = "#e0e0e0"
+                        fill = "none"
+                        cx = {s / 2}
+                        cy = {s / 2}
+                        r = {r}
+                        strokeWidth = {sw}
+                    />
+
+                    <Circle
+                        stroke = "#00C9A7"
+                        fill = "none"
+                        cx = {s / 2}
+                        cy = {s / 2}
+                        r = {r}
+                        strokeWidth ={sw}
+                        strokeDasharray = {`${p}, ${c}`}
+                        strokeLinecap = "round"
+                        rotation = "-90"
+                        origin = {`${s / 2}, ${s / 2}`}
+                    />
                 </Svg>
 
-                <View>
-                    <Text></Text>
+                <View style = {styles.center}>
+                    <Text style = {styles.bigScore}>{score}</Text>
                 </View>
             </View>
 
-            <Text></Text>
+            <Text style = {styles.feedback}></Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+
+})
